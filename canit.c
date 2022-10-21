3675461c274abfeef737462d3f1502db4a40206c
Raju-Bodige <116284989+Raju-Bodige@users.noreply.github.com>
2022-10-21 16:06:54 +0530

    Add files via upload

---
+++ canit.c
@@ -0,0 +1,13 @@
+/*
+ * File:   canit.c
+ * Author: rb65404
+ *
+ * Created on October 19, 2022, 10:25 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ newmain.c
@@ -0,0 +1,13 @@
+/*
+ * File:   newmain.c
+ * Author: rb65404
+ *
+ * Created on October 19, 2022, 10:23 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
